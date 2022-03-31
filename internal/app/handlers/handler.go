c6c6f0571bbbbcf989b4ffdb2a9915396622ff69
Aksarin Mikhail <m.aksarin@gmail.com>
2022-03-31 23:09:38 +0500

    Increment 4

--- internal/app/handlers/handler.go
+++ internal/app/handlers/handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -24,6 +25,7 @@ func NewMainHandler(repository storage.Repository, location string) *MainHandler
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Post("/", r.PostLongGetShort())
+	r.Post("/api/shorten", r.PostLongGetShortJSON())
 	r.Get("/{short}", r.GetLong())
 
 	return r
@@ -60,6 +62,43 @@ func (h *MainHandler) PostLongGetShort() http.HandlerFunc {
 		}
 		w.WriteHeader(http.StatusCreated)
 		_, err = fmt.Fprint(w, h.Location+string(shortURL))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Println("write answer error", err)
+			return
+		}
+	}
+}
+
+type PostLongJSONRequest struct {
+	URL storage.URL `json:"url"`
+}
+
+type PostLongJSONResponse struct {
+	Result storage.URL `json:"result"`
+}
+
+func (h *MainHandler) PostLongGetShortJSON() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var requestJSON PostLongJSONRequest
+		var responseJSON PostLongJSONResponse
+
+		if err := json.NewDecoder(r.Body).Decode(&requestJSON); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+		shortURL, err := h.Repository.SaveLongURL(requestJSON.URL)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Println("cant make short url", err)
+			return
+		}
+		responseJSON.Result = storage.URL(h.Location) + shortURL
+		w.WriteHeader(http.StatusCreated)
+		err = json.NewEncoder(w).Encode(responseJSON)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 			log.Println("write answer error", err)
--- internal/app/handlers/handler_test.go
+++ internal/app/handlers/handler_test.go
@@ -73,13 +73,24 @@ func TestMainHandler_ServeHTTP(t *testing.T) {
 			},
 		},
 		{
-			name:   "Test case #2",
+			name:   "Test case #3",
 			method: http.MethodGet,
 			target: "/c101c693",
 			want: want{
 				contentType: "text/plain; charset=utf-8",
 				statusCode:  404,
 				body:        "404 page not found\n",
+			},
+		},
+		{
+			name:        "Test case #4 with json",
+			method:      http.MethodPost,
+			target:      "/api/shorten",
+			requestBody: `{"url": "https://practicum.yandex.ru/learn/go-developer"}`,
+			want: want{
+				contentType: "application/json; charset=utf-8",
+				statusCode:  201,
+				body:        `{"result": "http://localhost:8080/8d34fd6f"}`,
 			},
 		},
 	}
@@ -112,8 +123,14 @@ func TestMainHandler_ServeHTTP(t *testing.T) {
 			assert.Equal(t, tt.want.contentType, resp.Header.Get("Content-Type"))
 
 			// получаем и проверяем тело запроса
-			assert.Equal(t, tt.want.body, respBody,
-				"Expected body [%s], got [%s]", tt.want.body, respBody)
+			switch resp.Header.Get("Content-Type") {
+			case "application/json; charset=utf-8":
+				assert.JSONEq(t, tt.want.body, respBody,
+					"Expected body [%s], got [%s]", tt.want.body, respBody)
+			default:
+				assert.Equal(t, tt.want.body, respBody,
+					"Expected body [%s], got [%s]", tt.want.body, respBody)
+			}
 
 		})
 	}
